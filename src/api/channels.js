dd8d7a4ef05dc83e2df9fc635428d792565c95c9
lishi-lin <675359876@qq.com>
2020-01-23 17:42:16 +0800

    可选频道渲染完成

--- src/api/channels.js
+++ src/api/channels.js
@@ -1,9 +1,15 @@
-// 用户相关的请求模块
+// 专门处理频道的请求
 import request from '@/utils/request' // 引入封装的模块
-// 标题 获取数据
+// 标题 获取我的频道数据
 export function getMyChannels () {
   // 返回一个Promise axios 默认就是get类型
   return request({
     url: '/user/channels'
+  })
+}
+// 获取所有频道
+export function getAllChannels () {
+  return request({
+    url: '/channels'
   })
 }
--- src/views/home/components/channel-edit.vue
+++ src/views/home/components/channel-edit.vue
@@ -7,18 +7,25 @@
         <van-button v-if="!editing" @click="editing=true" size="mini" type="info" plain>编辑</van-button>
         <van-button v-else @click="editing=false" size="mini" type="danger" plain>完成</van-button>
       </div>
+      <!-- 可选频道 -->
       <van-grid class="van-hairline--left">
-        <van-grid-item v-for="index in 8" :key="index">
-          <span class="f12">频道{{index}}</span>
-          <van-icon class="btn" name="cross"></van-icon>
+        <van-grid-item v-for="(channel,i) in channels" :key="channel.id">
+          <span class="f12">{{channel.name}}</span>
+          <!-- 通过编辑状态来控制叉号显示隐藏 -->
+          <template v-if="i!== 0">
+            <!-- 先控制频道是否等于0，在控制叉号显示隐藏 这样语义更好-->
+               <van-icon v-show="editing" class="btn" name="cross"></van-icon>
+          </template>
+          <!-- v-if 优先级大于v-show -->
+          <!-- <van-icon v-if="i!== 0" v-show="editing" class="btn" name="cross"></van-icon> -->
         </van-grid-item>
       </van-grid>
     </div>
     <div class="channel">
       <div class="tit">可选频道：</div>
       <van-grid class="van-hairline--left">
-        <van-grid-item v-for="index in 8" :key="index">
-          <span class="f12">频道{{index}}</span>
+        <van-grid-item v-for="channels in optionallChannels" :key="channels.id">
+          <span class="f12">{{channels.name}}</span>
           <van-icon class="btn" name="plus"></van-icon>
         </van-grid-item>
       </van-grid>
@@ -27,10 +34,34 @@
 </template>
 
 <script>
+import { getAllChannels } from '@/api/channels'
 export default {
   data () {
     return {
-      editing: false // 是否编辑
+      editing: false, // 是否编辑频道
+      allChannels: [] // 用来接收所有的频道
+    }
+  },
+  props: {
+    // 接收数据 我的频道
+    channels: {
+      type: Array,
+      default: () => [] //    eslint 要求我们必须用一个函数来生名一个数组函数
+    }
+  },
+  methods: {
+    async getAllChannels () {
+      const data = await getAllChannels()
+      this.allChannels = data.channels // 给所有频道赋值
+    }
+  },
+  created () {
+    this.getAllChannels()
+  },
+  computed: {
+    // 可选频道 = 全部频道 -  我的频道
+    optionallChannels () {
+      return this.allChannels.filter(item => !this.channels.some(o => o.id === item.id))
     }
   }
 }
